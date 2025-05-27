cbd0e69131318e19be72d3730ff964b29bec93b5
Stefan Jakovljevic <stefanjakovljevic@Julijas-MacBook-Pro.local>
2025-05-27 03:25:03 -0400

    adding docs actually

--- src/app/handler.rs
+++ src/app/handler.rs
@@ -2,6 +2,7 @@ use crate::app::App;
 use crossterm::event::{self, Event, KeyEvent, KeyEventKind};
 use std::io;
 
+/// Handles key events of [`App`].
 fn handle_key_event(app: &mut App, key_event: KeyEvent) {
     match key_event.code {
         crossterm::event::KeyCode::Char('q') => app.exit(),
@@ -9,6 +10,13 @@ fn handle_key_event(app: &mut App, key_event: KeyEvent) {
     }
 }
 
+/// Handles events of [`App`].
+///
+/// Checks for key events with the [`handle_key_event()`] function.
+///
+/// # Return
+/// This function returns an [`std::io::Result<()>`] depending on the [`crossterm::event::read()`]
+/// function.
 pub(super) fn handle_events(app: &mut App) -> io::Result<()> {
     match event::read()? {
         Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
--- src/app/mod.rs
+++ src/app/mod.rs
@@ -2,15 +2,29 @@ pub mod handler;
 use ratatui::{DefaultTerminal, Frame};
 use std::io;
 
+/// Stores Pinta's current state.
+///
+/// # Fields
+/// - `exit`: bool for exiting Pinta
 pub struct App {
     pub exit: bool,
 } // App{}
 
 impl App {
+    /// Creates a new instance of [`App`].
+    ///
+    /// This method initializes the App struct with default settings.
+    /// See the [`App`] struct for more information about its fields.
     pub fn new() -> Self {
         App { exit: false }
     } // App::new()
 
+    /// Runs the loop for [`App`].
+    ///
+    /// Until `self.exit`, the terminal will draw and handle events
+    ///
+    /// # Return
+    /// This function returns an [`std::io::Result`] depending on the loop's functionality
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.exit {
             terminal.draw(|frame| self.draw(frame))?;
